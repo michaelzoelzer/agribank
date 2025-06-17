c055445ccae182aef785e87f38c4a9c109fa5b4d
Michael <michael@metacity.com>
2025-06-17 12:23:18 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -26,13 +26,13 @@ IDEAL = {
 def get_soil(lat, lon):
     url = (
         f"https://rest.isric.org/soilgrids/v2.0/properties/query?lat={lat}&lon={lon}"
-        "&property=phh2o&property=sand&property=clay&depth=0-30cm&value=mean"
+        "&property=sand&property=clay&depth=0-30cm&value=mean"
     )
     r = requests.get(url)
     r.raise_for_status()
     d = r.json()["properties"]
     return {
-        "ph": d["phh2o"]["mean"],
+#        "ph": d["phh2o"]["mean"],
         "sand": d["sand"]["mean"],
         "clay": d["clay"]["mean"],
     }
